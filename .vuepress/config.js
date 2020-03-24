778c293a352f70854b0cdfdd40570ea59e53c8d7
WARHORSE-PRO <clintondsilva@fytinnovations.com>
2020-03-24 22:26:23 +0530

    add theme layout

---
+++ .vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    base: "/",
+    title: "OC-Careers",
+    description: "Careers Management Plugin for OctoberCMS",
+    themeConfig: {
+      sidebar: [
+        {
+          title: "Installation"
+        },
+        {
+          title: "UseCase"
+        },
+        {
+          title: "Components"
+        }
+      ],
+      nav: [
+        { text: "Guide", link: "/" },
+        {
+          text: "Marketplace",
+          link: "https://octobercms.com/plugin/fytinnovations-careers"
+        }
+      ],
+      docsRepo: "fytinnovations/oc-jobmanager",
+      editLinks: true,
+      editLinkText: "Help us improve this page!"
+    }
+  };
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Hello VuePress
+# OC Careers
+
+<blockquote>
+<p><strong>IMPORTANT:</strong> Get this plugin quick! Once the visual form builder is implemented in a future update the price will double to $100! Lock in your license now for half price!</p>
+</blockquote>
